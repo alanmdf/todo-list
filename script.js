7893ee75e028c6bfe6b916c2e445726224aec55c
alanmdf <alanmoreirascr@gmail.com>
2021-06-02 18:27:51 -0300

    Ajustes no lint

--- script.js
+++ script.js
@@ -1,27 +1,23 @@
-  window.onload = function() {
+window.onload = function () {
   const buttonCreateTask = document.getElementById('criar-tarefa');
   const inputBox = document.getElementById('texto-tarefa');
-  let taskList = document.getElementById('lista-tarefas');
+  const taskList = document.getElementById('lista-tarefas');
   const buttonClearList = document.getElementById('apaga-tudo');
-  const buttonRemoveFinishedItem = document.getElementById('remover-finalizados')
+  const buttonRemoveFinishedItem = document.getElementById('remover-finalizados');
 
-  buttonCreateTask.addEventListener('click', createTask);
-  taskList.addEventListener('click', changeTaskBGColor);
-  taskList.addEventListener('dblclick', strikeOutItem);
-  buttonClearList.addEventListener('click', clearList);
-  buttonRemoveFinishedItem.addEventListener('click', removeFinishedItem);
-  
   function createTask() {
-    let inputText = inputBox.value;
+    const inputText = inputBox.value;
     inputBox.value = '';
-    let itemList = document.createElement('li');
+    const itemList = document.createElement('li');
     itemList.className = 'list-item';
     itemList.innerText = inputText;
     taskList.appendChild(itemList);
   }
-  
-  function changeTaskBGColor(event) {    
-    let listItemsArray = document.getElementsByClassName('list-item');
+
+  buttonCreateTask.addEventListener('click', createTask);
+
+  function changeTaskBGColor(event) {
+    const listItemsArray = document.getElementsByClassName('list-item');
     for (let index = 0; index < listItemsArray.length; index += 1) {
       if (listItemsArray[index].classList.contains('selected')) {
         listItemsArray[index].classList.remove('selected');
@@ -32,27 +28,35 @@
       }
     }
   }
-  
+
+  taskList.addEventListener('click', changeTaskBGColor);
+
   function strikeOutItem(event) {
     if (event.target.classList.contains('completed')) {
       event.target.classList.remove('completed');
       event.target.style.textDecoration = 'none';
     } else {
       event.target.classList.add('completed');
-      event.target.style.textDecoration =  'line-through';
+      event.target.style.textDecoration = 'line-through';
     }
   }
+
+  taskList.addEventListener('dblclick', strikeOutItem);
 
   function clearList() {
     taskList.innerHTML = '';
   }
 
-// Para elaborar esta função abaixo, pesquisei e encontrei este link para usar de referência: https://stackoverflow.com/questions/44984867/javascript-remove-elements-by-class-name/44984940
+  buttonClearList.addEventListener('click', clearList);
+
+  // Para elaborar esta função abaixo, pesquisei e encontrei este link para usar de referência: https://stackoverflow.com/questions/44984867/javascript-remove-elements-by-class-name/44984940
 
   function removeFinishedItem() {
-    let completedItems = document.querySelectorAll('.completed');
-    for (let completed of completedItems) {
-      completed.remove();
+    const completedItems = document.querySelectorAll('.completed');
+    for (const completedItem of completedItems) {
+      completedItem.remove();
     }
   }
-}
+
+  buttonRemoveFinishedItem.addEventListener('click', removeFinishedItem);
+};
